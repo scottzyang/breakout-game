7afa5b5312efa5070230008df1f119b5b696bc2c
scottzyang <scottzyang@gmail.com>
2023-01-12 13:46:48 -0600

    Add collision detection

--- scripts.js
+++ scripts.js
@@ -5,10 +5,11 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+const ballRadius = 10;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD"
   ctx.fill();
   ctx.closePath();
@@ -20,6 +21,17 @@ function draw() {
   drawBall();
   x += dx;
   y += dy;
+
+  // reverse direction if top or bottom are hit
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  
+  // reverse direction if left or right are hit
+  if (y + dy > canvas.height - ballRadius|| y + dy < ballRadius) {
+    dy = -dy;
+  }
+  
 }
 // draw function will be executed every 10 ms
 setInterval(draw, 10)
